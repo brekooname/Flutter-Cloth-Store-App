59edcced90158f52be2c241da6c5af0f18f2f8b2
Sameera Perera <49270152+Sameera-Perera@users.noreply.github.com>
2022-08-08 12:24:13 +0530

    Update setting screen design

--- lib/presentation/screens/setting/setting_screen.dart
+++ lib/presentation/screens/setting/setting_screen.dart
@@ -26,11 +26,69 @@ class SettingScreen extends StatelessWidget {
                   style: Theme.of(context).textTheme.headline6,),
                   const Text("WandaRFincher@teleworm.us")
                 ],
-              )
+              ),
+
             ],
           ),
-        )
+        ),
+        const SizedBox(height: 30),
+        const SettingCard(
+          title: "Profile",
+        ),
+        const SizedBox(height: 6),
+        const SettingCard(
+          title: "Settings",
+        ),
+        const SizedBox(height: 6),
+        const SettingCard(
+          title: "Notifications",
+        ),
+        const SizedBox(height: 6),
+        const SettingCard(
+          title: "About",
+        ),
+        const SizedBox(height: 6),
+        const SettingCard(
+          title: "Sign Out",
+        ),
       ],
+    );
+  }
+}
+
+class SettingCard extends StatelessWidget {
+  final String title;
+  const SettingCard({
+    Key? key,
+    required this.title
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: InkWell(
+        onTap: (){
+
+        },
+        child: Card(
+          color: Colors.white,
+          elevation: 1,
+          clipBehavior: Clip.antiAlias,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 25),
+            child: Row(
+              children: [
+                Text(title,
+                style: Theme.of(context).textTheme.titleSmall,)
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
